c2207a0299baad3d16274efbd30b7558f16c861f
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-01-21 17:42:27 -0600

    creating core prefabs and scripts

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private States.PlayerState state = States.PlayerState.GROUNDED;
+    private string prev_type = "basic";
+
+    void Start()
+    {
+
+     
+    }
+
+    void OnPlayerLose(GameObject player)
+    {
+        if (player == gameObject)
+        {
+            // loseAnim.Play();
+            // switchCharacter(this, scene.characters);
+            // if (scene.state != States.GameState.GAME_OVER)
+            // {
+            //     scene.resetRound();
+            // }
+        }
+    }
+
+    void Update() {
+
+    }
+
+    private void Awake() {
+        
+    }
+}
