9c6a4b80a9fecd0fcc0b7d615a4343b5ca998e6c
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-01-23 17:07:16 -0600

    open/close doors via events

--- Assets/Scripts/Objects/Door.cs
+++ Assets/Scripts/Objects/Door.cs
@@ -7,6 +7,25 @@ public class Door : MonoBehaviour
     [SerializeField]
     private DoorState.Enums _state;
 
+    [SerializeField]
+    private GameEvents _events;
+
+    void Start() 
+    {
+        _events.activateDoorEvent.AddListener(Activate);
+    }
+
+    void Activate() 
+    {
+        if (_state == DoorState.Enums.OPEN) 
+        {
+            _state = DoorState.Enums.CLOSED;
+        } else 
+        {
+            _state = DoorState.Enums.OPEN;
+        }
+    }
+
     void Update() {
         if (_state == DoorState.Enums.OPEN) {
             gameObject.GetComponent<SpriteRenderer>().enabled = false;
