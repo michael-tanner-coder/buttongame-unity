afc92ba2268db6354bf062e6e5e9bcfcf4f2046a
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-01-24 16:33:14 -0600

    colliding bombs and items against doors

--- Assets/Item.cs
+++ Assets/Item.cs
@@ -4,6 +4,9 @@ public class Item : MonoBehaviour
 {
     [SerializeField]
     private ItemSO _itemData;
+    
+    [SerializeField]
+    private float _speed;
 
     void Start() 
     {
@@ -12,5 +15,13 @@ public class Item : MonoBehaviour
         renderer.sprite = _itemData.sprite;
 
         gameObject.name = _itemData.name;
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        if (other.gameObject.tag == "door") 
+        {
+            _speed *= -1;
+        }
     }
 }
--- Assets/Scripts/Objects/SmallBomb.cs
+++ Assets/Scripts/Objects/SmallBomb.cs
@@ -10,4 +10,12 @@ public class SmallBomb : MonoBehaviour
         float newX = transform.position.x + _speed * Time.deltaTime;
         transform.position = new Vector3(newX, transform.position.y, transform.position.z);
     }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        if (other.gameObject.tag == "door") 
+        {
+            _speed *= -1;
+        }
+    }
 }
