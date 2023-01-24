91796c464ae700c4ac15179ffb56116d1b1bf1bf
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-01-23 23:43:34 -0600

    new todo list

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -5,8 +5,32 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
+    // LOGIC
     // TODO: test movement between states for specific actions (timer runs out, animation ends, input is pressed, etc)
     // TODO: create feature toggles to enable/disable certain game elements (event types, obstacles, collectibles)
+    // TODO: spawn explosion object on round end
+    // TODO: create SOs for obstacles / collectibles
+    // TODO: spawn and move small bomb
+    // TODO: end round on collision with small bomb
+
+    // ANIMATION
+    // TODO: create countdown animation
+    // TODO: create bomb tween animation
+    // TODO: create player lose animation
+    // TODO: create door animation
+    // TODO: create explosion animation
+    
+    // GRAPHICS/UI
+    // TODO: display winner UI / get winning player
+    // TODO: adjust UI to fit display
+    // TODO: get pixel-perfect unity display
+
+    // SOUND
+    // TODO: use SOs to act as a sound manager
+    // TODO: load sound assets into SO
+    // TODO: play music on start
+    // TODO: switch music tempo based on player scores
+    // TODO: pair sounds with actions (jumping, landing, button press, tick, explode, etc)
 
     [SerializeField]
     private GameState.Enums state;
