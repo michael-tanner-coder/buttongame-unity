ed3685fcf705c9450c9dc8f61377c249d1d27fdd
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-01-29 18:57:24 -0600

    lifting code out of game manager

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -7,9 +7,6 @@ public class GameManager : MonoBehaviour
 
     // ARCHITECTURE (consider if necessary)
     // TODO: review unity architecture vid
-    // TODO: implement SO event architecture???
-    // TODO: implement SO variables
-    // TODO: address race conditions in event responses
     // TODO: provider public getters for SO values
 
     // LOGIC
@@ -23,7 +20,7 @@ public class GameManager : MonoBehaviour
     // TODO: create player lose animation
     // TODO: create door animation
     // TODO: create explosion animation
-    
+
     // GRAPHICS/UI
     // TODO: display winner UI / get winning player
     // TODO: adjust UI to fit display
@@ -37,101 +34,27 @@ public class GameManager : MonoBehaviour
     // TODO: pair sounds with actions (jumping, landing, button press, tick, explode, etc)
 
     [SerializeField]
-    private ScriptableObjectArchitecture.StateReference _state;
-    private bool roundEnded = false;
-
-    [SerializeField]
     private GameObjectCollection _playerPrefabs = default(GameObjectCollection);
 
     [SerializeField]
     private List<PlayerData> players;
 
-    [SerializeField]
-    private GameEvents _gameEvents;
-
-    [SerializeField]
-    private ScriptableObjectArchitecture.GameEvent testEvent;
-
-    [SerializeField]
-    private ScriptableObjectArchitecture.IntVariable _maxScore;
-
-    void Awake() 
+    void Awake()
     {
-        _gameEvents.roundEndEvent.AddListener(CheckForWinner);
         ResetPlayerData();
     }
 
     void ResetPlayerData()
     {
-        players.ForEach((PlayerData player) => {
+        players.ForEach((PlayerData player) =>
+        {
             player.ResetData();
         });
     }
-
-    PlayerData GetRoundWinner() 
-    {
-        PlayerData winner = null;
-        players.ForEach((PlayerData player) =>
-        {
-            if (player.doorState == DoorState.CLOSED)
-            {
-                // output winner
-                Debug.Log("Round Winner is " + player.playerName);
-
-                // update score
-                player.score.Value += 1;
-                winner = player;
-            }
-        });
-
-        return winner;
-    }
-
-
-    void CheckForMatchWinner(PlayerData winningPlayer) 
-    {
-        PlayerData matchWinner = null;
-
-        // if a player's score exceeds the max score, the game is over
-        if (isMatchWinner(winningPlayer))
-        {
-            // move to game over state
-            _gameEvents.gameOverEvent?.Invoke();
-            ResetPlayerData();
-
-            // output match winner
-            Debug.Log("Match Winner is " + winningPlayer.playerName);
-
-            matchWinner = winningPlayer;
-        }
-    
-        // if no one won the match, go back to round start state
-        if (matchWinner == null)
-        {
-            _gameEvents.roundStartEvent?.Invoke();
-        }
-    }
-
-    public void TestEvent()
-    {
-        Debug.Log("Test Event Raised");
-    }
-
-    void CheckForWinner() 
-    {
-        // if a player won the round, check if they also won the whole match
-        PlayerData roundWinner = GetRoundWinner();
-        if (roundWinner != null)
-        {
-            CheckForMatchWinner(roundWinner);
-            return;
-        }
-    
-        // otherwise, do another round
-        _gameEvents.roundStartEvent?.Invoke();
-    }
-    bool isMatchWinner(PlayerData player) 
-    {
-        return player.score.Value >= _maxScore.Value;
-    }
 }
+
+
+
+
+
+
