328090f8e5eca0eff9918910b8f0289b29dc051a
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-01-29 18:57:37 -0600

    getWinner script

--- Assets/Scripts/GetWinner.cs
+++ Assets/Scripts/GetWinner.cs
@@ -6,20 +6,53 @@ public class GetWinner : MonoBehaviour
     [SerializeField]
     private List<PlayerData> players;
 
+    [SerializeField]
+    private ScriptableObjectArchitecture.GameEvent startRound;
+
+    [SerializeField]
+    private ScriptableObjectArchitecture.GameEvent endMatch;
+
     public void OnRoundEnd()
     {
+        bool foundMatchWinner = false;
+
+        UpdateScores();
+
         players.ForEach((player) =>
         {
             if (isMatchWinner(player))
             {
                 Debug.Log("Winner!");
+                Debug.Log(player.playerName);
+                foundMatchWinner = true;
+                endMatch.Raise();
             }
-            else 
+        });
+        
+
+        if (!foundMatchWinner)
+        {
+            startRound.Raise();
+        }
+    }
+
+    void UpdateScores() 
+    {
+        PlayerData winner = null;
+        players.ForEach((PlayerData player) =>
+        {
+            if (player.doorState == DoorState.CLOSED)
             {
-                Debug.Log("Not Winner!");
+                // output winner
+                Debug.Log("Round Winner is " + player.playerName);
+
+                // update score
+                player.score.Value += 1;
+                winner = player;
             }
         });
     }
+
     bool isMatchWinner(PlayerData player) 
     {
         return player.score.Value >= player.maxScore.Value;
