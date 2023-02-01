6b188c9a936d7f7f6bfc17ce183c9b27830b1a34
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-02-01 00:34:26 -0600

    small bomb prefab

--- Assets/Scripts/Objects/SmallBomb.cs
+++ Assets/Scripts/Objects/SmallBomb.cs
@@ -1,21 +1,25 @@
 using UnityEngine;
+using ScriptableObjectArchitecture;
 
 public class SmallBomb : MonoBehaviour
 {
     [SerializeField]
-    private float _speed = 1f;
+    private FloatReference _speed;
+    
+    [SerializeField]
+    private GameObjectCollection _doors;
 
     void Update() 
     {
-        float newX = transform.position.x + _speed * Time.deltaTime;
+        float newX = transform.position.x + _speed.Value * Time.deltaTime;
         transform.position = new Vector3(newX, transform.position.y, transform.position.z);
     }
 
     void OnCollisionEnter2D(Collision2D other) 
     {
-        if (other.gameObject.tag == "door") 
+        if (_doors.Contains(other.gameObject)) 
         {
-            _speed *= -1;
+            _speed.Value *= -1;
         }
     }
 }
