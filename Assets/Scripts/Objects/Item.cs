e0b6b18ff61c28110471a8389482309676f1a511
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-02-01 18:54:46 -0600

    letting items pick their type on start

--- Assets/Scripts/Objects/Item.cs
+++ Assets/Scripts/Objects/Item.cs
@@ -1,25 +1,38 @@
 using UnityEngine;
+using ScriptableObjectArchitecture;
 
 public class Item : MonoBehaviour
 {
     [SerializeField]
     private ItemSO _itemData;
+
+    [SerializeField]
+    private ItemList _availableItems;
     
     [SerializeField]
     private float _speed;
 
+    [SerializeField]
+    private GameObjectCollection _doors;
+
+    public void PickItemType() 
+    {
+        int index = Random.Range(0, _availableItems.Value.Count);
+        _itemData = _availableItems.Value[index];
+    }
+
     void Start() 
     {
-        // read item data on start
+        // randomly pick an item to be on start
+        PickItemType();
         SpriteRenderer renderer = GetComponent<SpriteRenderer>();
         renderer.sprite = _itemData.sprite;
-
         gameObject.name = _itemData.name;
     }
 
     void OnCollisionEnter2D(Collision2D other) 
     {
-        if (other.gameObject.tag == "door") 
+        if (_doors.Contains(other.gameObject)) 
         {
             _speed *= -1;
         }
