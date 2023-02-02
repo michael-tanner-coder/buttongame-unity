7c44e4ab3e1df15d9fece5e7feea9354be5f2ed7
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-02-01 20:34:44 -0600

    changing height of door collider and sprite based on state

--- Assets/Scripts/Objects/Door.cs
+++ Assets/Scripts/Objects/Door.cs
@@ -13,6 +13,15 @@ public class Door : MonoBehaviour
     [SerializeField]
     private PlayerData _playerData;
 
+    private SpriteRenderer _spriteRenderer;
+    private BoxCollider2D _collider;
+
+    void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _collider = GetComponent<BoxCollider2D>();
+    }
+
     void Start() 
     {
         _events.activateDoorEvent.AddListener(Activate);
@@ -21,7 +30,17 @@ public class Door : MonoBehaviour
     void Activate() 
     {
         _state = _state == DoorState.CLOSED ? DoorState.OPEN : DoorState.CLOSED;
-        gameObject.GetComponent<SpriteRenderer>().enabled = _state == DoorState.CLOSED;
+        // _spriteRenderer.enabled = _state == DoorState.CLOSED;
         _playerData.doorState = _state;
+    }
+
+    void Update()
+    {
+        // change target height of sprite and box collider based on door state
+        float targetHeight = _state == DoorState.CLOSED ? 0.56f : 0f;
+        // 
+        _spriteRenderer.size = new Vector2(0.05f, targetHeight);
+        _collider.size = new Vector2(_spriteRenderer.size.x, _spriteRenderer.size.y);
+
     }
 }
