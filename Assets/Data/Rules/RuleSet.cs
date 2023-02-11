c3b833f5d624991f79c0e559b54560f0d38809cb
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-02-10 18:40:55 -0600

    added adjustable current values for each rule

--- Assets/Data/Rules/RuleSet.cs
+++ Assets/Data/Rules/RuleSet.cs
@@ -4,20 +4,28 @@ using ScriptableObjectArchitecture;
 [CreateAssetMenu(fileName = "RuleSet", menuName = "Rules/RuleSet", order = 0)]
 public class RuleSet : ScriptableObject {
     [Header("Gravity")]
-    [SerializeField] private FloatReference _gravityModifierDefault;
     [SerializeField] private FloatReference _gravityModifier;
+    [SerializeField] private FloatReference _defaultGravity;
+    [SerializeField] private float _currentGravityModifier;
     public FloatReference GravityModifier => _gravityModifier;
     
     [Header("Timer")]
-    [SerializeField] private FloatReference _tickRateModifierDefault;
     [SerializeField] private FloatReference _tickRateModifier;
+    [SerializeField] private FloatReference _defaultTickRate;
+    [SerializeField] private float _currentTickRateModifier;
     public FloatReference TickRateModifier => _tickRateModifier;
+
+    void OnValidate() 
+    {
+        _tickRateModifier.Value = _currentTickRateModifier;
+        _gravityModifier.Value = _currentGravityModifier;
+    }
 
     public void ResetToDefaults()
     {
         
-        _gravityModifier.Value = _gravityModifierDefault.Value;
-        _tickRateModifier.Value = _tickRateModifierDefault.Value;
+        _gravityModifier.Value = _defaultGravity.Value;
+        _tickRateModifier.Value = _defaultTickRate.Value;
     }
 
     public void ChangeRulesViaItem(ItemSO item) 
