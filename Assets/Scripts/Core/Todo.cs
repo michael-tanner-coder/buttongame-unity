c7d0c1bacd8b151e714244f7277c79401b9bb0c8
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-02-13 22:45:11 -0600

    fixing alignment bugs

--- Assets/Scripts/Core/Todo.cs
+++ Assets/Scripts/Core/Todo.cs
@@ -3,12 +3,10 @@ using UnityEngine;
 public class Todo : MonoBehaviour
 {
     // BUGS
-    // TODO: score bars are not correctly aligned
-    // TODO: scores ignore max score, only uses 1 as the limit
-    // TODO: player win text is always P2
     // TODO: countdown animation frames are unevenly spaced in time
-    // TODO: point signs are not aligned with station ceilings
     // TODO: buttons are not sticking out enough
+    // TODO: player win text is always P2
+    // TODO: scores ignore max score, only uses 1 as the limit
     
     // LOGIC
     // TODO: end round on collision with small bomb
