416217cf644f7d18c949eab9ef68061be6a744c8
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-02-13 22:36:33 -0600

    new bug todo list

--- Assets/Scripts/Core/Todo.cs
+++ Assets/Scripts/Core/Todo.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class Todo : MonoBehaviour
 {
+    // BUGS
+    // TODO: score bars are not correctly aligned
+    // TODO: scores ignore max score, only uses 1 as the limit
+    // TODO: player win text is always P2
+    // TODO: countdown animation frames are unevenly spaced in time
+    // TODO: point signs are not aligned with station ceilings
+    // TODO: buttons are not sticking out enough
+    
     // LOGIC
-    // TODO: restart match with enter key
     // TODO: end round on collision with small bomb
     // TODO: respawn missing players on round start
 
