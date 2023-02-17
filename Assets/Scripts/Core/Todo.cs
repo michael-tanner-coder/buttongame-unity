a4023c49031e452674430a391cbb4f1c51fcab57
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-02-16 22:26:32 -0600

    Update Todo.cs

--- Assets/Scripts/Core/Todo.cs
+++ Assets/Scripts/Core/Todo.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class Todo : MonoBehaviour
 {
     // BUGS
-    // TODO: player win text is always P2
     // TODO: scores ignore max score, only uses 1 as the limit
     // TODO: buttons are not sticking out enough (need to re-scale game)
     
