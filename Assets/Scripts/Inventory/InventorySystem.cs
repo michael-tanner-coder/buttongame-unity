101e09e3fb5bb3f9ccbd9afa4a5ad0d7124de268
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-02-19 16:44:03 -0600

    inventory system for item slots and stacks

---
+++ Assets/Scripts/Inventory/InventorySystem.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySystem : MonoBehaviour
+{
+    private Dictionary<ItemSO, InventoryItem> m_itemDictionary;
+    [SerializeField] private ItemList _dataList;
+    [SerializeField] public List<InventoryItem> inventory { get; private set; }
+
+    private void Awake()
+    {
+        inventory = new List<InventoryItem>();
+        m_itemDictionary = new Dictionary<ItemSO, InventoryItem>();
+        foreach(ItemSO item in _dataList.Value)
+        {
+            Add(item);
+        }
+    }
+
+    public void Add(ItemSO referenceData)
+    {
+        if (m_itemDictionary.TryGetValue(referenceData, out InventoryItem value))
+        {
+            value.AddToStack();
+        }
+        else
+        {
+            InventoryItem newItem = new InventoryItem(referenceData);
+            inventory.Add(newItem);
+            m_itemDictionary.Add(referenceData, newItem);
+        } 
+    }
+
+    public void Remove(ItemSO referenceData)
+    {
+        if (m_itemDictionary.TryGetValue(referenceData, out InventoryItem value))
+        {
+            value.RemoveFromStack();
+
+            if (value.stackSize == 0)
+            {
+                inventory.Remove(value);
+                m_itemDictionary.Remove(referenceData);
+            }
+        }
+    }
+}
