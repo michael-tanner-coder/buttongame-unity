f27713d2e17811de9d0b943207869fc901146937
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-02-20 14:05:21 -0600

    WIP: discarding items for roulette effects

--- Assets/Roulette.cs
+++ Assets/Roulette.cs
@@ -6,12 +6,19 @@ public class Roulette : MonoBehaviour
     [SerializeField] private FloatVariable _speed;
     [SerializeField] private IntVariable _money;
     [SerializeField] private IntVariable _cost;
+    [SerializeField] private ItemList _selectedItems;
+    [SerializeField] private ItemList _inventory;
     public void ToggleRoulette()
     {
         if (_speed.Value == 0 && _money.Value >= _cost.Value)
         {
             _money.Value -= _cost.Value;
             _speed.Value = 240f;
+            foreach(ItemSO item in _selectedItems.Value)
+            {
+                Debug.Log("Discarding " + item.itemName);
+                _inventory.Value.Remove(item);
+            }
             // emit event that roulete finished?
         }
         else 
