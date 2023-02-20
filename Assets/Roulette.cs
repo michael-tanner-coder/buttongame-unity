fa3a48eb127db47da18d9551782a99e44567fd65
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-02-20 13:42:34 -0600

    player can pay currency for a roulette spin

--- Assets/Roulette.cs
+++ Assets/Roulette.cs
@@ -4,8 +4,19 @@ public class Roulette : MonoBehaviour
 {
 
     [SerializeField] private FloatVariable _speed;
+    [SerializeField] private IntVariable _money;
+    [SerializeField] private IntVariable _cost;
     public void ToggleRoulette()
     {
-        _speed.Value = _speed.Value == 0f ? 240f : 0f;
+        if (_speed.Value == 0 && _money.Value >= _cost.Value)
+        {
+            _money.Value -= _cost.Value;
+            _speed.Value = 240f;
+            // emit event that roulete finished?
+        }
+        else 
+        {
+            _speed.Value = 0f;
+        }
     }
 }
