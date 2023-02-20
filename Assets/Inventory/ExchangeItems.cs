909f7fef13aca5989ae1f371a7d0154750178121
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-02-20 17:08:05 -0600

    removing item from inventory on exchange

--- Assets/Inventory/ExchangeItems.cs
+++ Assets/Inventory/ExchangeItems.cs
@@ -5,12 +5,14 @@ public class ExchangeItems : MonoBehaviour
 {
     [SerializeField] private ItemList _selectedItems;
     [SerializeField] private IntVariable _money;
+    [SerializeField] private Inventory _inventorySystem;
 
     public void Exchange()
     {
         foreach(ItemSO item in _selectedItems.Value)
         {
             _money.Value += item.value;
+            _inventorySystem.Remove(item);
         }
     }
 
