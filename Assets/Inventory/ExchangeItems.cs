a8e8626983aed779246e2fd46d3c56df75deefa9
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-02-20 17:12:07 -0600

    clearing out selected item on exchange and awake

--- Assets/Inventory/ExchangeItems.cs
+++ Assets/Inventory/ExchangeItems.cs
@@ -7,12 +7,18 @@ public class ExchangeItems : MonoBehaviour
     [SerializeField] private IntVariable _money;
     [SerializeField] private Inventory _inventorySystem;
 
+    
+    public void Awake()
+    {
+            _selectedItems.Value.Clear();
+    }
     public void Exchange()
     {
         foreach(ItemSO item in _selectedItems.Value)
         {
             _money.Value += item.value;
             _inventorySystem.Remove(item);
+            _selectedItems.Value.Clear();
         }
     }
 
