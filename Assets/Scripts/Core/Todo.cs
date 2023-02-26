09a52f29af2236ab7ca1baeb6642cb6c3ec7d82b
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-02-25 21:55:08 -0600

    Update Todo.cs

--- Assets/Scripts/Core/Todo.cs
+++ Assets/Scripts/Core/Todo.cs
@@ -2,22 +2,12 @@ using UnityEngine;
 
 public class Todo : MonoBehaviour
 {
-    // BUGS
-    // TODO: sometimes players don't spawn on game start
-    
-    // LOGIC
-    // TODO: end round on collision with small bomb
-    // TODO: respawn missing players on round start
-
-    // ANIMATION
-    // TODO: create door animation
-    // TODO: create small bomb roll animation
-    // TODO: create player lose animation
-
-    // SOUND
-    // TODO: use SOs to act as a sound manager
-    // TODO: load sound assets into SO
-    // TODO: play music on start
-    // TODO: switch music tempo based on player scores
-    // TODO: pair sounds with actions (jumping, landing, button press, tick, explode, etc)
+    // TODO: write game instructions
+    // TODO: write item descriptions
+    // TODO: make item icons
+    // TODO: test item drop rates
+    // TODO: test each item effect
+    // TODO: make inventory UI refresh
+    // TODO: fix player movement
+    // TODO: polish UI placement/size
 }
