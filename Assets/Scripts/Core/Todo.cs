05ab6643c1208d3b9d74bbbca861e43f74199e2c
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-02-26 19:56:35 -0600

    writing game instructions

--- Assets/Scripts/Core/Todo.cs
+++ Assets/Scripts/Core/Todo.cs
@@ -10,4 +10,20 @@ public class Todo : MonoBehaviour
     // TODO: make inventory UI refresh
     // TODO: fix player movement
     // TODO: polish UI placement/size
+
+    // GOAL: 
+    // keep winning at a roulette game to reach a certain amount of casino chips
+
+    // CONTROLS:
+    // press UP or W to jump
+    // click items and buttons with left mouse button
+
+    // HOW TO PLAY:
+    // jump to hit the button to start up a roulette spin (this will cost some chips)
+    // while the roulette is spinning, you can hit the button again to stop it
+    // if an item lands inside the roulette hitbox, it will be added to your inventory
+    // any item in you have can be used up to alter the properties of the roulette (speed, hitbox size, available items, etc)
+    // to use an item, click it and press the Use Items button. You can use up to 3 items per spin.
+    // OR: you can sell items to get more chips. Rare items will have more powerful effects and are worth more chips
+    // as you gain more chips, the roulette spins get faster and will cost more chips
 }
