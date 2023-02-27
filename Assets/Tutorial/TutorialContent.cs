fa6679a72004f4672a2f0b7004f60f5ec0b8d15d
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-02-26 20:44:13 -0600

    script for setting tutorial prompt content

---
+++ Assets/Tutorial/TutorialContent.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TutorialContent : MonoBehaviour
+{
+    [SerializeField] private TutorialPrompt _prompt;
+    [SerializeField] private TMP_Text _header;
+    [SerializeField] private GameObject _contentBox;
+    [SerializeField] private Image _image;
+
+    void Start()
+    {
+        _header.text = _prompt.Header;
+        _image = _prompt.Image;
+        _contentBox.GetComponent<ContentList>().list = _prompt.Content;
+    }
+}
