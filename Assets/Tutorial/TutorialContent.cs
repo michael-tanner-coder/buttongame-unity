8d326750d056d0838a2bda0926d89a1899b8c3b5
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-02-27 18:02:08 -0600

    methods for moving between tutorial prompts

--- Assets/Tutorial/TutorialContent.cs
+++ Assets/Tutorial/TutorialContent.cs
@@ -1,18 +1,44 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
 
 public class TutorialContent : MonoBehaviour
 {
-    [SerializeField] private TutorialPrompt _prompt;
+    [SerializeField] private List<TutorialPrompt> _prompts = new List<TutorialPrompt>();
+    private int _index = 0;
     [SerializeField] private TMP_Text _header;
     [SerializeField] private GameObject _contentBox;
     [SerializeField] private Image _image;
 
     void Start()
     {
-        _header.text = _prompt.Header;
-        _image = _prompt.Image;
-        _contentBox.GetComponent<ContentList>().InitList(_prompt.Content);
+        UpdatePrompt();
+    }
+    public void UpdatePrompt()
+    {
+        _header.text = _prompts[_index].Header;
+        _image = _prompts[_index].Image;
+        _contentBox.GetComponent<ContentList>().InitList(_prompts[_index].Content);
+    }
+
+    public void NextPrompt()
+    {
+        _index++;
+        if (_index >= _prompts.Count)
+        {
+            _index = _prompts.Count - 1;
+        }
+        UpdatePrompt();
+    }
+
+    public void PreviousPrompt()
+    {
+        _index--;
+        if (_index <= 0)
+        {
+            _index = 0;
+        }
+        UpdatePrompt();
     }
 }
